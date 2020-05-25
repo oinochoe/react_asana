40162a2ad1538a5ea5b2034934d522db1add3413
yeongminkim <oinochoe@naver.com>
2020-05-26 01:34:36 +0900

    프로젝트 세팅

---
+++ src/pages/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../components/common/Button';
+
+const TodoList = () => {
+    return (
+        <div>
+            <Button>등록</Button>
+        </div>
+    );
+};
+
+export default TodoList;
